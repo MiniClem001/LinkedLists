10a97042dd7366f4fda5a35f57ced441437ee453
Clément Giraud-Sauveur <git@milouki.ovh>
2025-02-23 16:30:40 +0100

    création d'un fichier d'en-tête et ajout d'une fonction d'initialisation

---
+++ liste.h
@@ -0,0 +1,19 @@
+#ifndef LISTE_H
+#define LISTE_H
+
+typedef struct Element Element;
+struct Element
+{
+    int nombre;
+    Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste
+{
+    Element *premier;
+};
+
+Liste *initialisation();
+
+#endif
--- main.c
+++ main.c
@@ -1,14 +1,25 @@
-typedef struct Element
-{
-    int nombre;
-    Element *suivant;
-} Element;
-
-typedef struct Liste
-{
-    Element *premier;
-} Liste;
+#include <stdio.h>
+#include <stdlib.h>
+#include "liste.h"
 
 int main(int argc, char *argv[])
 {
+    Liste *liste = initialisation();
+}
+
+Liste *initialisation()
+{
+    Liste *liste = malloc(sizeof(*liste));
+    Element *element = malloc(sizeof(*element));
+
+    if (liste == NULL || element == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    element->nombre = 0;
+    element->suivant = NULL;
+    liste->premier = element;
+
+    return liste;
 }
