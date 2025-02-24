bb4d4d1a61f149138546378e4887fef7583708e7
Clément Giraud-Sauveur <git@milouki.ovh>
2025-02-24 10:42:58 +0100

    refactorisation en fichier liste_chainee.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "liste.h"
+#include "liste_chainee.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,68 +14,4 @@ int main(int argc, char *argv[])
     afficher(liste);
 
     return 0;
-}
-
-Liste *initialisation()
-{
-    Liste *liste = malloc(sizeof(*liste));
-    Element *element = malloc(sizeof(*element));
-
-    if (liste == NULL || element == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    element->nombre = 0;
-    element->suivant = NULL;
-    liste->premier = element;
-
-    return liste;
-}
-
-void insertion(Liste *liste, int nvNombre)
-{
-    // Création d'un nouvel élément
-    Element *nouveau = malloc(sizeof(*nouveau));
-    if (liste == NULL || nouveau == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
-    nouveau->nombre = nvNombre;
-
-    // Insertion de l'élément au début de la liste
-    nouveau->suivant = liste->premier;
-    liste->premier = nouveau;
-}
-
-void suppression(Liste *liste)
-{
-    if (liste == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    if (liste->premier != NULL)
-    {
-        Element *aSupprimer = liste->premier;
-        liste->premier = aSupprimer->suivant;
-        free(aSupprimer);
-    }
-}
-
-void afficher(Liste *liste)
-{
-    if (liste == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
-
-    Element *actuel = liste->premier;
-
-    while (actuel != NULL)
-    {
-        printf("%d -> ", actuel->nombre);
-        actuel = actuel->suivant;
-    }
-    printf("NULL\n");
 }
